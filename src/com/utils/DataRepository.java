3a5683eebc7ee2e198f49ccbe8bc1cf3b1d649cb
wolodya <wolodya@GLaDOS.aperture.lab>
2025-09-22 16:50:41 +0500

    add char ? after char =

--- src/com/utils/DataRepository.java
+++ src/com/utils/DataRepository.java
@@ -4,13 +4,12 @@ import java.util.Map;
 
 public class DataRepository {
     public Map<String, Integer> QA;
-	
 	public DataRepository() {
 		QA = new HashMap<>() {{
-			put("1 + 1 =", 2);
-			put("2 + 5 =", 7);
-			put("9 + 0 =", 9);
-			put("33 + 28 =", 61);
+			put("1 + 1 = ?", 2);
+			put("2 + 5 = ?", 7);
+			put("9 + 0 = ?", 9);
+			put("33 + 28 = ?", 61);
 
-	} 
+	}
 }
