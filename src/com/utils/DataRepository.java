de138545c79f0ba3c7e648d4f8ae0d910c3d90dc
wolodya <wolodya@GLaDOS.aperture.lab>
2025-09-22 17:26:54 +0500

    For DataRepository add } to end

--- src/com/utils/DataRepository.java
+++ src/com/utils/DataRepository.java
@@ -3,17 +3,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DataRepository {
-	private final Map<String, Integer> QA;
-	
-	public DataRepository() {
-		QA =  Map.of(
-			"1 + 1 = ?", 2,
-			"4 + 5 = ?", 9,
-			"33 + 28 = ?", 61
-		);
+    private final Map<String, Integer> QA;
 
-	} 
+    public DataRepository() {
+        QA = Map.of(
+                "1 + 1 = ?", 2,
+                "4 + 5 = ?", 9,
+                "33 + 28 = ?", 61
+        );
 
-	public Map<String, Integer> getQA() {
-		return QA;
+    }
+
+    public Map<String, Integer> getQA() {
+        return QA;
+    }
 }
